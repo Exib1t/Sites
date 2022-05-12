8adc4f1929556be27364bbd2713718632435718f
Exib1t <dmaznyak2604@gmail.com>
2022-05-12 22:37:10 +0300

    UpVHS Hamburger added

--- UpVHS/dev/src/js/main.js
+++ UpVHS/dev/src/js/main.js
@@ -2,6 +2,7 @@
 
 $(function() {
   // initFunctionExample();
+  hamburger();
 });
 
 $(window).on('resize', function() {
@@ -17,3 +18,28 @@ function initFunctionExample() {
 
 }
 */
+
+function hamburger() {
+  const hamburger = document.querySelector('.hamburger');
+  const list = document.querySelector('.b-mainNavigation');
+
+  hamburger.addEventListener('click', toggleClass);
+
+  function toggleClass() {
+    hamburger.classList.toggle('is-active');
+    list.classList.toggle('-state-open');
+    list.classList.remove('-state-close');
+
+    if (!hamburger.classList.contains('is-active')) {
+      list.classList.add('-state-close');
+    }
+  }
+
+  window.onresize = function () {
+    if (window.innerWidth > 945) {
+      document.querySelector('.hamburger').classList.remove('is-active');
+      document.querySelector('.b-mainNavigation').classList.remove('-state-open');
+      document.querySelector('.b-mainNavigation').classList.remove('-state-close');
+    }
+  };
+}
--- UpVHS/inc/header.php
+++ UpVHS/inc/header.php
@@ -63,6 +63,11 @@
             </li>
           </ul>
         </nav>
+        <button class="hamburger hamburger--elastic" type="button">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
       </div>
     </header>
 
