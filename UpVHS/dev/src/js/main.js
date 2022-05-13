77615a4831bc757c36cef9884f7d71019b5e5ca2
Exib1t <dmaznyak2604@gmail.com>
2022-05-14 00:26:15 +0300

    UpVHS hamburger fix

--- UpVHS/dev/src/js/main.js
+++ UpVHS/dev/src/js/main.js
@@ -19,6 +19,31 @@ function initFunctionExample() {
 }
 */
 
+// function hamburger() {
+//   const hamburger = document.querySelector('.hamburger');
+//   const list = document.querySelector('.b-mainNavigation');
+//
+//   hamburger.addEventListener('click', toggleClass);
+//
+//   function toggleClass() {
+//     hamburger.classList.toggle('is-active');
+//     list.classList.toggle('-state-open');
+//     list.classList.remove('-state-close');
+//
+//     if (!hamburger.classList.contains('is-active')) {
+//       list.classList.add('-state-close');
+//     }
+//   }
+//
+//   window.onresize = function () {
+//     if (window.innerWidth > 945) {
+//       document.querySelector('.hamburger').classList.remove('is-active');
+//       document.querySelector('.b-mainNavigation').classList.remove('-state-open');
+//       document.querySelector('.b-mainNavigation').classList.remove('-state-close');
+//     }
+//   };
+// }
+
 function hamburger() {
   const hamburger = document.querySelector('.hamburger');
   const list = document.querySelector('.b-mainNavigation');
@@ -28,18 +53,12 @@ function hamburger() {
   function toggleClass() {
     hamburger.classList.toggle('is-active');
     list.classList.toggle('-state-open');
-    list.classList.remove('-state-close');
-
-    if (!hamburger.classList.contains('is-active')) {
-      list.classList.add('-state-close');
-    }
   }
 
   window.onresize = function () {
     if (window.innerWidth > 945) {
       document.querySelector('.hamburger').classList.remove('is-active');
       document.querySelector('.b-mainNavigation').classList.remove('-state-open');
-      document.querySelector('.b-mainNavigation').classList.remove('-state-close');
     }
   };
 }
