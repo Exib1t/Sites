c9b0e8d42a07f705bd0e836814979c50827f22de
Exib1t <dmaznyak2604@gmail.com>
2022-06-01 00:05:27 +0300

    UpVHS Adaptive fixes

--- UpVHS/dev/src/js/main.js
+++ UpVHS/dev/src/js/main.js
@@ -28,9 +28,19 @@ function hamburger() {
   function toggleClass() {
     hamburger.classList.toggle('is-active');
     list.classList.toggle('-state-open');
+    scrollDisable();
+  }
+
+  function scrollDisable() {
+    if(hamburger.classList.contains('is-active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }
 
   window.onresize = function () {
+    scrollDisable();
     if (window.innerWidth > 945) {
       document.querySelector('.hamburger').classList.remove('is-active');
       document.querySelector('.b-mainNavigation').classList.remove('-state-open');
