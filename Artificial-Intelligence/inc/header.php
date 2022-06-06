d1f0c321389eb48b321ad8dc05a5f7b70cbbf93b
Exib1t <dmaznyak2604@gmail.com>
2022-06-07 01:35:53 +0300

    Artificial Intelligence header&content

---
+++ Artificial-Intelligence/inc/header.php
@@ -0,0 +1,91 @@
+<?php
+  if ( empty( $_SERVER['HTTPS'] ) || $_SERVER['HTTPS'] === 'off' ) {
+    $protocol = 'http://';
+  } else {
+    $protocol = 'https://';
+  }
+  $BASE_URL = $protocol . $_SERVER['SERVER_NAME'] . dirname( $_SERVER['PHP_SELF'] );
+
+  $isHomepage = ( basename( $_SERVER['PHP_SELF'] ) == 'index.php' );
+  $title = $isHomepage ? $siteName : $pageName . ' : ' . $siteName;
+?>
+<!doctype html>
+<html class="l-html no-js" lang="">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="" />
+
+  <!-- <meta property="og:image" content="<?php echo $BASE_URL; ?>/img/userfiles/og-image.png" /> -->
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <link rel="shortcut icon" href="favicon.ico" />
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600;700;800&display=swap" rel="stylesheet">
+
+  <!--<meta name="theme-color" content="#ed1c24" />-->
+
+  <link rel="stylesheet" href="assets/css/main.min.css?<?php echo filemtime( 'assets/css/main.min.css' ); ?>" />
+</head>
+<body class="l-body -page_<?php echo $uri; ?><?php echo ( ! $isHomepage ) ? ' -page_inner' : ''; ?>">
+
+  <div class="l-wrapper">
+    <header class="l-siteHeader">
+      <div class="b-siteHeader">
+        <div class="l-siteLogo">
+          <?php
+            $siteLogo__tag  = $isHomepage ? 'h1' : 'div';
+            $siteLogo__link = $isHomepage ? '' : ' href="index.html"';
+          ?>
+          <<?php echo $siteLogo__tag; ?> class="b-siteLogo" itemscope itemtype="http://schema.org/Organization">
+            <a class="b-siteLogo__link"<?php echo $siteLogo__link; ?> itemprop="url">
+              <img class="b-siteLogo__icon" src="assets/img/blocks/siteLogo/siteLogo-logo.svg" alt="<?php echo $siteName; ?>" title="<?php echo $siteName; ?>" itemprop="logo" />
+            </a>
+          </<?php echo $siteLogo__tag; ?>>
+        </div>
+
+        <nav class="l-mainNavigation">
+          <ul class="b-mainNavigation">
+            <li class="b-mainNavigation__item<?php echo ( $uri == 'home' ) ? ' -state_active' : ''; ?>">
+              <a class="b-mainNavigation__link" href="index.html">Home</a>
+            </li>
+            <li class="b-mainNavigation__item<?php echo ( $uri == 'about' ) ? ' -state_active' : ''; ?>">
+              <a class="b-mainNavigation__link" href="index.html">About</a>
+            </li>
+            <li class="b-mainNavigation__item<?php echo ( $uri == 'services' ) ? ' -state_active' : ''; ?>">
+              <a class="b-mainNavigation__link" href="index.html">Services</a>
+            </li>
+            <li class="b-mainNavigation__item<?php echo ( $uri == 'blog' ) ? ' -state_active' : ''; ?>">
+              <a class="b-mainNavigation__link" href="index.html">Blog</a>
+            </li>
+            <li class="b-mainNavigation__item<?php echo ( $uri == 'contact' ) ? ' -state_active' : ''; ?>">
+              <a class="b-mainNavigation__link" href="index.html">Contact</a>
+            </li>
+          </ul>
+
+          <div class="l-mainAuth">
+            <ul class="b-mainAuth">
+              <li class="b-mainAuth__item<?php echo ( $uri == 'contact' ) ? ' -state_active' : ''; ?>">
+                <a class="b-mainAuth__link" href="index.html">Sign In</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <button class="hamburger hamburger--collapse" type="button">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+
+      </div>
+    </header>
+
+    <div class="l-content">
---
+++ Artificial-Intelligence/index.php
@@ -0,0 +1,52 @@
+<?php
+$pageName = 'Home';
+$siteName = 'Artificial Intelligence';
+$uri      = 'home';
+require( 'inc/header.php' );
+?>
+
+<main class="l-contentText">
+  <section class="l-firstScreen">
+    <div class="b-firstScreen">
+      <div class="b-firstScreen__side">
+        <h2 class="b-firstScreen__sideSubtitle">Next genaretion platform</h2>
+        <h3 class="b-firstScreen__sideTitle">Artificial intelligence & Syber security</h3>
+        <div class="b-firstScreen__sideText b-text">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+        <div class="b-firstScreen__sideButtons">
+          <a href="#" class="b-firstScreen__sideButton">Get Started</a>
+          <a href="#" class="b-firstScreen__sideButton"><span></span>Watch Video</a>
+        </div>
+      </div>
+      <div class="b-firstScreen__side">
+        <div class="b-firstScreen__sideImgHolder">
+          <img src="assets/img/blocks/firstScreen/firstScreen-img.svg" alt="" class="b-firstScreen__sideImg">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="l-contentLogo">
+    <div class="b-contentLogo">
+      <img src="assets/img/blocks/contentLogo/logo_1.svg" alt="" class="b-contentLogo__logo">
+      <img src="assets/img/blocks/contentLogo/logo_2.svg" alt="" class="b-contentLogo__logo">
+      <img src="assets/img/blocks/contentLogo/logo_3.svg" alt="" class="b-contentLogo__logo">
+      <img src="assets/img/blocks/contentLogo/logo_4.svg" alt="" class="b-contentLogo__logo">
+    </div>
+  </section>
+
+  <section class="l-contentInfo">
+    <div class="b-contentInfo">
+      <div class="b-contentInfo__imgHolder">
+        <img src="assets/img/blocks/contentInfo/image_1.svg" alt="" class="b-contentInfo__imgHolderImg">
+      </div>
+      <div class="b-contentInfo__side">
+        <h3 class="b-contentInfo__sideTitle">Apply AI, Deep Learning and Data Sciece to solve</h3>
+        <div class="b-contentInfo__sideText b-text">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
+        <a href="#" class="b-contentInfo__sideButton">Learn More</a>
+      </div>
+    </div>
+  </section>
+
+</main>
+
+<?php require( 'inc/footer.php' ); ?>
